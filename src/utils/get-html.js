5de7d6c3167f9fcb510cb177b984c6231583c452
Tariq Al Fayad <mail@ashiknesin.com>
2019-08-11 21:53:00 +0530

    Remove raw field from getHTML

--- src/utils/get-html.js
+++ src/utils/get-html.js
@@ -16,77 +16,75 @@ const puppeteer = require('puppeteer-core');
 // const localChromePath = `/Applications/Google Chrome.app/Contents/MacOS/Google Chrome`;
 
 const puppeteerDefaultConfig = {
-    headless: true,
-    args: [
-        '--no-sandbox',
-        '--disable-setuid-sandbox',
-        '--disable-gpu',
-        '--start-maximized',
-        '--single-process',
-        `--window-size=1200,900` // you can also use '--start-fullscreen'
-    ]
+  headless: true,
+  args: [
+    '--no-sandbox',
+    '--disable-setuid-sandbox',
+    '--disable-gpu',
+    '--start-maximized',
+    '--single-process',
+    `--window-size=1200,900`, // you can also use '--start-fullscreen'
+  ],
 };
 
 const getHTML = async (
-    targetUrl,
-    {
-        prerender = false,
-        // proxyUrl=null,
-        puppeteerOpts = {}
-        // prerenderOpts = {},
-        // requestConfig = {}
-    } = {}
+  targetUrl,
+  {
+    prerender = false,
+    // proxyUrl=null,
+    puppeteerOpts = {},
+    // prerenderOpts = {},
+    // requestConfig = {}
+  } = {}
 ) => {
-    ow(targetUrl, ow.string.not.empty);
-    const payload = {
-        html: null,
-        statusCode: null,
-        raw: null,
-        errorMsg: null
-    };
+  ow(targetUrl, ow.string.not.empty);
+  const payload = {
+    html: null,
+    statusCode: null,
+    errorMsg: null,
+  };
 
-    if (!prerender) {
-        const response = await axios.get(targetUrl);
-        payload.raw = response;
-        payload.html = response.data;
-        payload.statusCode = response.status;
-        return payload;
-    }
-
-    ow(puppeteerOpts.executablePath, ow.string.not.empty);
-
-    let browser = null;
-
-    try {
-        browser = await puppeteer.launch({
-            ...puppeteerDefaultConfig,
-            ...puppeteerOpts
-        });
-        console.log('spawning chrome headless');
-
-        const page = await browser.newPage();
-        await page.setViewport({
-            height: 900,
-            width: 1200
-        });
-
-        await page.goto(targetUrl, {
-            waitUntil: ['domcontentloaded']
-        });
-        payload.html = await page.content();
-        browser.close();
-    } catch (error) {
-        // console.log('error', error);
-        payload.errorMsg = error.message;
-    } finally {
-        // close browser
-        if (browser !== null) {
-            console.log('Closing the headless browser');
-            await browser.close();
-        }
-    }
-
+  if (!prerender) {
+    const response = await axios.get(targetUrl);
+    payload.html = JSON.stringify(response.data);
+    payload.statusCode = response.status;
     return payload;
+  }
+
+  ow(puppeteerOpts.executablePath, ow.string.not.empty);
+
+  let browser = null;
+
+  try {
+    browser = await puppeteer.launch({
+      ...puppeteerDefaultConfig,
+      ...puppeteerOpts,
+    });
+    console.log('spawning chrome headless');
+
+    const page = await browser.newPage();
+    await page.setViewport({
+      height: 900,
+      width: 1200,
+    });
+
+    await page.goto(targetUrl, {
+      waitUntil: ['domcontentloaded'],
+    });
+    payload.html = await page.content();
+    browser.close();
+  } catch (error) {
+    // console.log('error', error);
+    payload.errorMsg = error.message;
+  } finally {
+    // close browser
+    if (browser !== null) {
+      console.log('Closing the headless browser');
+      await browser.close();
+    }
+  }
+
+  return payload;
 };
 
 export default getHTML;
